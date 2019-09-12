f0ec8282b8e1b8b043ac82c09033d3efca7ce7fd
ajimenezdev <ajmjimens@gmail.com>
2019-09-12 16:51:30 +0200

    Update footer

--- augur-react-test/src/components/footer/index.js
+++ augur-react-test/src/components/footer/index.js
@@ -1,8 +1,37 @@
 import React from "react";
+import { Icon, IconButton } from "@material-ui/core";
+
 import "./footer.css";
 
 const Footer = () => {
-  return <footer className="footer">Footer</footer>;
+  return (
+    <footer className="footer">
+      <p className="footer-author">
+        created by:
+        <a
+          href="https://alvarojimenezmartin.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Alvaro Jimenez Martin
+        </a>
+      </p>
+      <IconButton
+        aria-label="open"
+        onClick={() =>
+          window.open(
+            "https://github.com/ajimenezdev/augur-front-test",
+            "_blank"
+          )
+        }
+      >
+        <div className="footer-sourceContainer">
+          <Icon className="footer-sourceIcon">code</Icon>
+          <span className="footer-sourceText">Source Code</span>
+        </div>
+      </IconButton>
+    </footer>
+  );
 };
 
 export default Footer;
